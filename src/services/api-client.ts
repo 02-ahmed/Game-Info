06b6f3020ade662a32f07d555fe0b0c05b7fbd83
02-ahmed <ahmedboabae2@gmail.com>
2024-07-07 02:50:08 +0000

    fetched the genres

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'f371911cc60a48deb7be643aa5799df9 b'
+    key: 'f371911cc60a48deb7be643aa5799df9'
   }
 })
